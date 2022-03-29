a496a57b34fddd305fc0476210525fab211394b7
pratkpranav <cs1180368@iitd.ac.in>
2022-03-29 14:03:25 +0530

    should work now

--- server.js
+++ server.js
@@ -38,7 +38,7 @@ app.use(cors())
 app.use((serveStatic(__dirname + "/FormsApp/dist")));
 
 app.get('/', function(req,res){
-    res.render('index.html');
+    res.sendFile(path.join(__dirname + "/FormsApp/dist" + '/index.html'));
 })
 
 let totalslides = 4;
